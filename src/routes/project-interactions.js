f588631cfcf22f4e835ad48c2292f5a7a6925b2b
jagadish1211 <mrjagadish1211@gmail.com>
2022-01-05 14:06:57 +0530

    Added post api for interactions

--- src/routes/project-interactions.js
+++ src/routes/project-interactions.js
@@ -1,29 +1,47 @@
-const ProjectInteractions = require('../models/project-interactions');
+const ProjectInteractions = require("../models/project-interactions");
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next)=>{
-    const query = req.query;
-    let filterQuery = {}; // For all case
-    if(query['project'] && query['project'] != '__all__'){
-        filterQuery['project'] = query['project']; // For specific project 
-    }
-    ProjectInteractions.find(filterQuery).then(data=>{
-        res.json({
-            message: 'Successful',
-            data: data,
-        })
-    }).catch(err=>{
-        res.status(400).json({
-            message: err ? err.message : 'Internal error',
-        })
+router.get("/", (req, res, next) => {
+  const query = req.query;
+  let filterQuery = {}; // For all case
+  if (query["project"] && query["project"] != "__all__") {
+    filterQuery["project"] = query["project"]; // For specific project
+  }
+  ProjectInteractions.find(filterQuery)
+    .then((data) => {
+      res.json({
+        message: "Successful",
+        data: data,
+      });
     })
+    .catch((err) => {
+      res.status(400).json({
+        message: err ? err.message : "Internal error",
+      });
+    });
 });
 
-router.post('/', (req, res, next)=>{
-    res.json({
-        message: "Interactions post called"
+router.post("/", (req, res, next) => {
+  const body = req.body;
+  console.log(body);
+  ProjectInteractions.findOneAndUpdate(
+    {
+      project: body.project,
+    },
+    { $inc: { interactions: 1 } }
+  )
+    .then((data) => {
+      res.json({
+        message: "Interactions post called",
+        data: data,
+      });
     })
+    .catch((err) => {
+      res.status(400).json({
+        message: "Interactions post called",
+      });
+    });
 });
 
 module.exports.router = router;
